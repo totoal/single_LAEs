28dbb832438ba46a4ac53b4ea5c57440ed5dcf5e
totoal <ttalbertott@gmail.com>
2023-01-03 17:02:17 +0100

    New files

---
+++ load_single_cats.py
@@ -0,0 +1,36 @@
+import pandas as pd
+
+def load_s_cats(cat_list=['minijpas', 'jnep']):
+    '''
+    Loads the single mode catalogs of miniJPAS and J-NEP
+    (specify one or both, default=both).
+    '''
+    # Column names
+    columns = ['tileid', 'filterid', 'flags', 'number', 'RA', 'DEC',
+               'x_im', 'y_im', 'background', 'threshold', 'flux',
+               'flux_relerr', 'mask_flags']
+    types_list = ['int', 'int', 'int', 'int', 'float', 'float', 'float',
+                  'float', 'int', 'int', 'float', 'float', 'int']
+    types_dict = {}
+    for colname, dt in zip(columns, types_list):
+        types_dict[colname] = dt
+
+    merged_cat = pd.DataFrame([])
+
+    for cat_name in cat_list:
+        cat = pd.read_csv(f'csv/{cat_name}.Flambda_single.csv', header=1,
+                          names=columns)
+        cat = cat.astype(types_dict)
+
+        # Convert fluxes to erg s^-1 cm^-2 units and relerr to err
+        cat['flux'] = cat['flux'] * 1e-19
+        cat['flux_err'] = cat['flux_relerr'] * cat['flux']
+        cat = cat.drop('flux_relerr', axis=1)
+
+        merged_cat = pd.concat([merged_cat, cat])
+
+    return merged_cat
+
+
+if __name__ == '__main__':
+    load_s_cats()
